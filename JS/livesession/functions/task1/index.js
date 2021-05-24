cd3d3a9f3f263d510549085ff32b57f04af1c1e5
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-05-24 15:50:48 +0300

    tet1

--- JS/livesession/functions/task1/index.js
+++ JS/livesession/functions/task1/index.js
@@ -8,9 +8,15 @@ function getSquared(num) {
     return num ** 2;
 }
 // Long option
+
+
 const result = getSquared(30);
 console.log(result);
+
+
 // Short option
+
+
 console.log(getSquared(30));
 // ================
 function sum(a, b) {
@@ -18,8 +24,10 @@ function sum(a, b) {
 }
 
 function greeting(age) {
-    console.log(`I am ${age} years old`);
+    console.log(`I am ' + ' years old`);
 }
+
+
 // long option
 const someFunc = (num1, num2) => {
     return num1 * num2;
@@ -28,8 +36,10 @@ const someFunc = (num1, num2) => {
 const mult = (a, b) => a * b;
 // ==============================
 const greetingExpression = function(age) {
-    console.log(`I am ${age} years old`);
+    console.log(`I am ' + age + ' years old`);
 };
 greetingExpression(50);
+
+
 // ===============================
 const getSquared = num => num ** 2;
