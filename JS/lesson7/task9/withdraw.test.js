4cb3b36a890d19c374a9e0acd9b40e582f0eb790
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-05-25 23:21:15 +0300

    test2

--- JS/lesson7/task9/withdraw.test.js
+++ JS/lesson7/task9/withdraw.test.js
@@ -1,16 +1,16 @@
 import { withdraw } from './withdraw';
 
 it('should have the right name', () => {
-    const result = withdraw([clients, balances, client, amount]);
-    expect(result).toEqual([clients, balances, client, amount]);
+    const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10);
+    expect(result).toEqual(-1, [1400, 87, -6]);
 })
 
 it('should withdraw amount if enough money', () => {
     const result = withdraw(['Ann', 'John'], [87, -6], 'Ann', 50);
-    expect(result).toEqual([37, -6]);
+    expect(result).toEqual(37);
 })
 
 it('should not withdraw money if amount > balance', () => {
     const result = withdraw(['Ann', 'John'], [30, -6], 'Ann', 50);
-    expect(result).toEqual([30, -6]);
+    expect(result).toEqual(-1, [30, -6]);
 })
