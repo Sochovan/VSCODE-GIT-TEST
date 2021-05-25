262eb9ae8edf17f0dd7c500372b03cf08cc98f40
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-05-25 18:42:57 +0300

    test1

--- JS/lesson12/task3/index.js
+++ JS/lesson12/task3/index.js
@@ -1,17 +1,6 @@
 const cleanTransactionsList = arr => {
-    // let str = arr.join();
-    let resArray = [];
+    const resArray = arr.filter(elem => isFinite(elem)).map(elem => '$' + parseFloat(elem).toFixed(2));
 
-    for (let i = 0; i < arr.length; i++) {
-        if (Number(arr[i]) > 0) {
-            resArray.push(arr[i]);
-        }
-        let str = resArray.join();
-
-        let result = str.split('').trim;
-        console.log(result);
-    }
     return resArray;
 }
-
 console.log(cleanTransactionsList(['  1.9 ', '16.4', 17, '1 dollar ']));
