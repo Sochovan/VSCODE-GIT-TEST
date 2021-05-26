5a12031c2133587409d09f27fae339ecffb003f8
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-05-26 14:10:31 +0300

    еуіе1

--- JS/lesson14/task2/index.js
+++ JS/lesson14/task2/index.js
@@ -32,37 +32,48 @@
 // //если перенести let message = 'Hi!' внутрь функции createMessage, то
 // // Ann, Hi!. You Gromecode
 
-// let message = 'Just learn it!';
+
+
+let message = 'Just learn it!';
+let sender = 'Gromcode';
 
 const createMessenger = () => {
-    let message = 'Just learn it!';
+
+
 
     function sendMessage(name) {
-        const sender = 'Gromecode';
-        console.log(`${name}, ${message} Your ${sender}`);
+
+        console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     function setMessage(text) {
         message = text;
-
     }
 
     function setSender(sign) {
         sender = sign;
     }
+
     return {
-        sendMesssage: sendMessage, //sendMessage
-        setMessage: setMessage,
-        setSender: setSender, //setMessage потому что одинаковый ключ и значение
+        sendMessage,
+        setMessage,
+        setSender,
     };
+
 };
+
+
+// export default createMessenger;
 
 const messenger1 = createMessenger();
 
-messenger1.sendMesssage('Bob'); //Bob, Hi!. You Gromecode
+messenger1.sendMessage('Bob');
 
 const messenger2 = createMessenger();
-messenger2.setMessage('Hello!'); //Tom, Hi!. You Gromecode
-messenger2.setMessage('');
-// messenger2.setMessage('Hello!');
-// messenger1.sendMesssage('Ann'); //Ann, Hello!. You Gromecode - потому что функция сохраняет ссылку на внешнее лексическое окружение в момент ее объявления
+messenger2.setMessage('Good job!');
+messenger1.sendMessage('Bob');
+
+const messenger3 = createMessenger();
+
+messenger3.setSender('Gromcode1');
+messenger1.sendMessage('Bob');
