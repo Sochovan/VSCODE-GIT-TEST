30c265d75ad7fe6cb650292cfe3d612ba54a8b6a
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-02 23:44:23 +0300

    test1

---
+++ JS/lesson21/task9/index.js
@@ -0,0 +1,19 @@
+export { finishList };
+
+const finishList = () => {
+  const list = document.querySelector('ul');
+  const parentElem = document.ul;
+  const firstItem = document.createElement('li');
+  firstItem.textContent = '1';
+  document.ul.prepend(firstItem);
+  const fourthItem = document.createElement('li');
+  fourthItem.textContent = '4';
+  const fifthItem = document.querySelector('.special');
+  fifthItem.before(fourthItem);
+  const sixthItem = document.createElement('li');
+  sixthItem.textContent = '6';
+  fifthItem.after(sixthItem);
+  const eightthItem = document.createElement('li');
+  eightthItem.textContent = '8';
+  parentElem.appendChild(eightthItem);
+};
