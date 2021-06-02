9ea33b6fbc63ebb65be981618426655847cca0cc
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-02 18:26:42 +0300

    test1

---
+++ JS/lesson21/task4/index.js
@@ -0,0 +1,21 @@
+export { getTitle, getDescription, getPlans, getGoal };
+
+const getTitle = () => {
+  const text = document.querySelector('.title');
+  return text.textContent();
+};
+
+const getDescription = () => {
+  const description = document.querySelector('.about');
+  return description.innerText();
+};
+
+const getPlans = () => {
+  const plan = document.querySelector('.plans');
+  return plan.innerHTML();
+};
+
+const getGoal = () => {
+  const goal = document.querySelector('.goal');
+  return goal.outerHTML();
+};
