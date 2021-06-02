70ce93267543ca5b6657898f19a86a26be006d8f
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-02 17:59:24 +0300

    test1

---
+++ JS/lesson21/task2/index.js
@@ -0,0 +1,15 @@
+export { getTitleElement, getInputElement };
+
+const getTitleElement = () => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+  return titleElem;
+};
+
+const getInputElement = () => {
+  const inputElem = document.querySelector('input');
+  console.dir(inputElem);
+  return inputElem;
+};
+
+console.log(titleElem);
