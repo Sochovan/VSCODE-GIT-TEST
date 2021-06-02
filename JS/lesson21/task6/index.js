d9a0dcd4b7ce7899fa82ca908d200948ebead7e9
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-02 23:02:18 +0300

    test10

--- JS/lesson21/task6/index.js
+++ JS/lesson21/task6/index.js
@@ -2,7 +2,8 @@ export { setButton };
 
 const setButton = buttonText => {
   const container = document.querySelector('body');
-  const parentElement = document.body;
+  const parentElem = document.body;
   const button = document.createElement('button');
   button.innerHTML = buttonText;
+  parentElem.appendChild(button);
 };
