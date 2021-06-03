b922c689bea661d1819d0c70d8119f6b1063ee42
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-03 22:53:58 +0300

    test12

--- JS/lesson21/task10/index.js
+++ JS/lesson21/task10/index.js
@@ -6,7 +6,18 @@ const finishForm = () => {
   firstInput.setAttribute('type', 'password');
   const input = document.createElement('input');
 
-  input.name = 'login';
-  form.append(input);
+  input.setAttribute('type', 'text');
+  input.setAttribute('name', 'login');
+  form.prepend(input);
 };
 // console.log(finishForm());
+
+// export const finishForm = () => {
+//   // const newInputElement = document.createElement('input');
+//   // newInputElement.setAttribute('type', 'text');
+//   // newInputElement.setAttribute('name', 'login');
+//   // const formElementOne = document.querySelector('.login-form');
+//   // formElementOne.prepend(newInputElement);
+//   // const formElementTwo = document.querySelector('input[name="password"]');
+//   // formElementTwo.setAttribute('type', 'password');
+// };
