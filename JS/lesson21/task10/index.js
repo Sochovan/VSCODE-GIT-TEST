ff53754abdd60c286e2ca1f24175ed626f0c7c28
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-04 15:22:59 +0300

    test1

--- JS/lesson21/task10/index.js
+++ JS/lesson21/task10/index.js
@@ -10,14 +10,4 @@ const finishForm = () => {
   input.setAttribute('name', 'login');
   form.prepend(input);
 };
-// console.log(finishForm());
-
-// export const finishForm = () => {
-//   // const newInputElement = document.createElement('input');
-//   // newInputElement.setAttribute('type', 'text');
-//   // newInputElement.setAttribute('name', 'login');
-//   // const formElementOne = document.querySelector('.login-form');
-//   // formElementOne.prepend(newInputElement);
-//   // const formElementTwo = document.querySelector('input[name="password"]');
-//   // formElementTwo.setAttribute('type', 'password');
-// };
+console.log(finishForm());
---
+++ JS/lesson21/task11/index.js
@@ -0,0 +1,14 @@
+export { manageClasses };
+
+const manageClasses = () => {
+  const elem1 = document.querySelector('.one');
+  elem1.classList.add('selected');
+  const elem2 = document.querySelector('.two');
+  elem2.classList.remove('selected');
+  const elem3 = document.querySelector('.three');
+  elem3.classList.toggle('three_done');
+  const elem4 = document.getElementsByClassName('.some-class');
+  elem4.classList.add('another-class');
+};
+
+// console.log(manageClasses());
