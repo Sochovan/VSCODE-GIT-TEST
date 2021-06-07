2e2e5b9916a1a7dae722d681fbd79fb1f4e292be
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-07 08:27:49 +0300

    test17

--- JS/lesson21/task11/index.js
+++ JS/lesson21/task11/index.js
@@ -1,16 +1,15 @@
 export { manageClasses };
 
 const manageClasses = () => {
-  const elem1 = document.gquerySelector('one');
+  const elem1 = document.querySelector('one');
   elem1.classList.add('.selected');
   const elem2 = document.querySelector('.two');
   elem2.classList.remove('selected');
   const elem3 = document.querySelector('.three');
   elem3.classList.toggle('three_done');
   const elem4 = document.querySelector('.four');
-  if (elem4.classList.contains('some-class')) {
-    elem4.classList.add('another-class');
-  }
+  elem4.classList.contains('.some-class');
+  elem4.classList.add('another-class');
 };
 
 // console.log(manageClasses());
