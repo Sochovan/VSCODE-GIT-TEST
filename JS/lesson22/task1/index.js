8272b6d3ca7d9caba2270681a4aedf4951eeb7fb
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-07 13:25:34 +0300

    test1

--- JS/lesson22/task1/index.js
+++ JS/lesson22/task1/index.js
@@ -16,42 +16,52 @@
 
 // elem.addEventListener('click', handler);
 // elem.removeEventListener('click', handler);
+
 const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
+const eventsListElem = document.querySelector('.events-list');
+
 const logTarget = (text, color) => {
-  const eventsListElem = document.querySelector('.events-list');
-  eventsListElem.innerHTML += `<span style="color: ${color}; margin-left: 8px">${text}</span>`;
+  eventsListElem.innerHTML += `<span style ='color: ${color}; margin-left:8px;'>${text}</span>`;
 };
+
 const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
 const logGreenP = logTarget.bind(null, 'P', 'green');
 const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
 const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
-const clearButton = document.querySelector('.clear-btn');
+
+const clear = document.querySelector('.clear-btn');
 const clearList = () => {
-  const eventsListElem = document.querySelector('.events-list');
   eventsListElem.innerHTML = '';
 };
-clearButton.addEventListener('click', clearList);
-const attachHandlersBtn = document.querySelector('.attach-handlers-btn');
-const attachHandlers = () => {
-  divElem.addEventListener('click', logGreyDiv, true);
+
+clear.addEventListener('click', clearList);
+const attachHandlers = document.querySelector('.attach-handlers-btn');
+const attach = () => {
+  divElem.addEventListener('click', logGreyDiv, { capture: true }); //тоже самое что и третьим аргументом добавить true
   divElem.addEventListener('click', logGreenDiv);
+
   pElem.addEventListener('click', logGreyP, true);
   pElem.addEventListener('click', logGreenP);
+
   spanElem.addEventListener('click', logGreySpan, true);
   spanElem.addEventListener('click', logGreenSpan);
 };
-attachHandlersBtn.addEventListener('click', attachHandlers);
-const removeHandlersBtn = document.querySelector('.remove-handlers-btn');
-const removeHandlers = () => {
-  divElem.removeEventListener('click', logGreyDiv, true);
+attachHandlers.addEventListener('click', attach);
+
+const removeHandlers = document.querySelector('.remove-handlers-btn');
+const remove = () => {
+  divElem.removeEventListener('click', logGreyDiv, true); //тоже самое что и третьим аргументом добавить true
   divElem.removeEventListener('click', logGreenDiv);
+
   pElem.removeEventListener('click', logGreyP, true);
   pElem.removeEventListener('click', logGreenP);
+
   spanElem.removeEventListener('click', logGreySpan, true);
   spanElem.removeEventListener('click', logGreenSpan);
 };
-removeHandlersBtn.addEventListener('click', removeHandlers);
+removeHandlers.addEventListener('click', remove);
---
+++ JS/lesson22/task2/index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('.create-btn');
+
+const pushButton = () => {
+  alert('done');
+};
+
+button.addEventListener('click', pushButton);
