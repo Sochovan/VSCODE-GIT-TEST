a89bfabe6c511798687ecc0aab69ea56d5405681
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-07 12:35:44 +0300

    еуые2

--- JS/lesson22/task1/index.js
+++ JS/lesson22/task1/index.js
@@ -20,10 +20,9 @@
 const divElem = document.querySelector('.rect_div');
 const pElem = document.querySelector('.rect_p');
 const spanElem = document.querySelector('.rect_span');
+const eventsListElem = document.querySelector('.events-list');
 
 const logTarget = (text, color) => {
-  const eventsListElem = document.querySelector('.events-list');
-
   eventsListElem.innerHTML += `<span style ='color: ${color}; margin-left:8px;'>${text}</span>`;
 };
 
@@ -35,15 +34,32 @@ const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
 const logGreyP = logTarget.bind(null, 'P', 'grey');
 const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
 
-divElem.addEventListener('click', logGreyDiv, { capture: true }); //тоже самое что и третьим аргументом добавить true
-pElem.addEventListener('click', logGreyP, true);
-spanElem.addEventListener('click', logGreySpan, true);
-
-divElem.addEventListener('click', logGreenDiv);
-pElem.addEventListener('click', logGreenP);
-spanElem.addEventListener('click', logGreenSpan);
-
 const clear = document.querySelector('.clear-btn');
-clear.removeEventListener('click', logGreenDiv);
-clear.removeEventListener('click', logGreenP);
-clear.removeEventListener('click', logGreenSpan);
+const clearList = () => {
+  eventsListElem.innerHTML = '';
+};
+
+clear.addEventListener('click', clearList);
+const attachHandlers = document.querySelector('.attach-handlers-btn');
+const attach = () => {
+  divElem.addEventListener('click', logGreyDiv, { capture: true }); //тоже самое что и третьим аргументом добавить true
+  pElem.addEventListener('click', logGreyP, true);
+  spanElem.addEventListener('click', logGreySpan, true);
+
+  divElem.addEventListener('click', logGreenDiv);
+  pElem.addEventListener('click', logGreenP);
+  spanElem.addEventListener('click', logGreenSpan);
+};
+attachHandlers.addEventListener('click', attach);
+
+const removeHandlers = document.querySelector('.remove-handlers-btn');
+const remove = () => {
+  divElem.removeEventListener('click', logGreyDiv, { capture: true }); //тоже самое что и третьим аргументом добавить true
+  pElem.removeEventListener('click', logGreyP, true);
+  spanElem.removeEventListener('click', logGreySpan, true);
+
+  divElem.removeEventListener('click', logGreenDiv);
+  pElem.removeEventListener('click', logGreenP);
+  spanElem.removeEventListener('click', logGreenSpan);
+};
+removeHandlers.addEventListener('click', remove);
