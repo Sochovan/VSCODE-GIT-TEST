7f34830525205281f2c61ea89cfa554d4ba112a7
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-23 16:05:30 +0300

    test2

--- JS/lesson34/task3/index.js
+++ JS/lesson34/task3/index.js
@@ -2,13 +2,15 @@ const baseUrl = 'https://60d1e1ef5b017400178f49d0.mockapi.io/api/v1/users';
 
 const button = document.querySelector('.submit-button');
 const form = document.querySelector('.login-form');
-const inputs = document.querySelector('.form-input');
+const inputEmail = document.querySelector('#email');
+const inputName = document.querySelector('#name');
+const inputPasword = document.querySelector('#password');
 const error = document.querySelector('.error-text');
 // console.dir(button);
 
 // button activated
 const onValidateForm = () => {
-  if (form.reportValidity() === true) {
+  if (form.reportValidity()) {
     button.disabled = false;
   }
 };
@@ -16,25 +18,39 @@ onValidateForm();
 
 form.addEventListener('click', onValidateForm);
 
-// // get data fron server
-// const getFormData = () => {
-//   return fetch(baseUrl)
-//     .then(response => response.json())
-//     .then(result => console.log(result));
-// };
+// get data for server
+const getFormData = event => {
+  event.preventDefault();
+  const formData = [...new FormData(form)].reduce(
+    (acc, [field, value]) => ({
+      ...acc,
+      [field]: value,
+    }),
+    {},
+  );
+
+  alert(JSON.stringify(formData));
+  inputEmail.value = '';
+  inputName.value = '';
+  inputPasword.value = '';
+};
+
+form.addEventListener('submit', getFormData);
 
 // console.log(getFormData());
 
-// // post data on server
-// const postOnServer = formData => {
-//   return fetch(baseUrl, {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json;charset=utf-8',
-//     },
-//     body: JSON.stringify(formData),
-//   });
-// };
-// inputs.innerHTML = '';
-// form.addEventListener('click', postOnServer);
-// // postOnServer({ email: 'nas@gmail.com', username: 'dispatchEvent', password: '4354' });
+// post data on server
+
+const postOnServer = formData => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(formData),
+  });
+};
+
+form.addEventListener('click', postOnServer);
+
+// postOnServer({ email: 'nas@gmail.com', username: 'dispatchEvent', password: '4354' });
