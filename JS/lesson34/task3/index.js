db320d993f4575161d500c695e4aa8e3feedfa50
Sochovan <80044602+Sochovan@users.noreply.github.com>
2021-06-23 22:44:55 +0300

    test14

--- JS/lesson34/task3/index.js
+++ JS/lesson34/task3/index.js
@@ -44,7 +44,7 @@ const getFormData = event => {
     headers: {
       'Content-Type': 'application/json;charset=utf-8',
     },
-    body: JSON.stringify(formData),
+    body: JSON.parse(formData),
   })
     .then(response => response.json())
     .then(result => {
